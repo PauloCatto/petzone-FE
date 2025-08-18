91b2b6c8850bb687ce8120cc608a2feb127bfb3a
Paulo Catto <paulohcatto@hotmail.com>
2025-08-17 22:58:40 -0300

    update: fix format

--- src/app/services/pet.service.ts
+++ src/app/services/pet.service.ts
@@ -11,13 +11,11 @@ export class PetService {
   private url: string = environment.url;
 
   constructor(private http: HttpClient) {}
-
   getPets(): Observable<{ products: Pet[]; backgroundAsset: string }> {
     return this.http.get<{ products: Pet[]; backgroundAsset: string }>(
       `${this.url}/home`
     );
   }
-
   getTypes(type: Pet['type']): Observable<Pet[]> {
     const path = type === 'fish' ? 'fishes' : `${type}s`;
     return this.http.get<Pet[]>(`${this.url}/${path}`);
